d252ecfcbd6e31502a33cf37b26451fd97a71e48
Tim Leverett <zzzzbov@gmail.com>
2019-12-14 17:21:35 -0500

    styled tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Tabs, TabList, Tab, TabPanels, TabPanel } from '@reach/tabs'
 import { Page, LoginForm, RegistrationForm } from './components'
 
@@ -7,13 +7,18 @@ export interface IAppProps {
 }
 
 export const App: React.FC<IAppProps> = () => {
+  const [tab, setTab] = useState(0)
 
   return (
     <Page>
-      <Tabs className='Tabs'>
+      <Tabs className='Tabs' index={tab} onChange={setTab}>
         <TabList className='TabList'>
-          <Tab className='Tab'>Log in</Tab>
-          <Tab className='Tab'>Register</Tab>
+          <Tab className={`Tab Tab-${tab === 0 ? 'active' : 'inactive'}`}>
+            Log in
+          </Tab>
+          <Tab className={`Tab Tab-${tab === 1 ? 'active' : 'inactive'}`}>
+            Register
+          </Tab>
         </TabList>
         <TabPanels className='TabPanels'>
           <TabPanel className='TabPanel'>
