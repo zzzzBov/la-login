17dc61be03d98fc5e676bdf89d66df7f53d0cb1d
Tim Leverett <zzzzbov@gmail.com>
2019-12-16 12:35:06 -0500

    update greeting to autofocus

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useRef, useEffect } from 'react'
 
 export interface IGreetingProps {
   onLogOut (): void
@@ -9,9 +9,17 @@ export const Greeting: FC<IGreetingProps> = ({
   onLogOut,
   username
 }) => {
+  const heading = useRef<HTMLHeadingElement>(null)
+
+  useEffect(() => {
+    if (heading.current) {
+      heading.current.focus()
+    }
+  }, [heading])
+
   return (
     <div className='Greeting'>
-      <h1>Welcome {username}!</h1>
+      <h1 ref={heading} tabIndex={-1}>Welcome {username}!</h1>
       <button className='Greeting_logOut' type='button' onClick={onLogOut}>Log Out</button>
     </div>
   )
